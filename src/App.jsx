b6ad01ec49e99394e87601a70258219336aeae86
Tyra <68145311+Tyrarose@users.noreply.github.com>
2024-05-26 15:02:51 +0800

    touches foot

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Header from "./components/Header";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import fireproshieldImage from "./images/fireproshield.png";
+import facebookIcon from "./images/fb.png";
+import twitterIcon from "./images/twitter.png";
+import instagramIcon from "./images/ig.png";
+import flameoutlogo from "./images/flamoutlogo.png";
 
 function App() {
 	const handleScrollToExtinguishers = () => {
@@ -56,6 +60,89 @@ function App() {
 							>
 								View extinguishers
 							</button>
+						</div>
+					</div>
+				</div>
+				<div className="foot">
+					<div className="col-md-10">
+						{" "}
+						{/* Take most of the width */}
+						<div className="social-icons float-md-right">
+							{" "}
+							<a
+								href="https://www.facebook.com"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<img src={facebookIcon} alt="Facebook" />
+							</a>
+							<a
+								href="https://www.twitter.com"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<img src={twitterIcon} alt="Twitter" />
+							</a>
+							<a
+								href="https://www.instagram.com"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<img src={instagramIcon} alt="Instagram" />
+							</a>
+						</div>
+					</div>
+					<div className="col-md-2 col-sm-3">
+						<div className="flameout_logo">
+							<img src={flameoutlogo} alt="Instagram" />
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="row frame_two">
+				<div className="red_border">
+					<div className="col-12">
+						<div className="introduction">
+							<h1>
+								<strong>Our Extinguishers</strong>
+							</h1>
+							<h4>
+								Lorem ipsum dolor sit amet, consectetur
+								adipiscing elit.
+							</h4>
+						</div>
+
+						<div className="row imageandtext">
+							<div className="col-5">
+								<img
+									src="https://cpworldgroup.com/wp-content/uploads/2021/01/placeholder.png"
+									alt=""
+									style={{
+										maxWidth: "100%",
+										height: "auto",
+										display: "block",
+									}}
+								/>
+							</div>
+							<div className="col-7">
+								<p>
+									Lorem ipsum dolor sit amet, consectetur
+									adipiscing elit, sed do eiusmod tempor
+									incididunt ut labore et dolore magna aliqua.
+									Est ante in nibh mauris cursus mattis
+									molestie a. Facilisis gravida neque
+									convallis a cras. Pellentesque habitant
+									morbi tristique senectus. Egestas purus
+									viverra accumsan in nisl nisi scelerisque.
+									Eget mauris pharetra et ultrices neque
+									ornare aenean. Cursus euismod quis viverra
+									nibh cras pulvinar mattis. Ipsum faucibus
+									vitae aliquet nec ullamcorper sit amet.
+									Magnis dis parturient montes nascetur
+									ridiculus mus mauris vitae ultricies.
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
