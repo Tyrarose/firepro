1285423354d610ee0ab26cda65eeee8745a45514
Tyra <68145311+Tyrarose@users.noreply.github.com>
2024-06-22 13:34:53 +0800

    fixed mobile

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
-
-import "../styles/AboutUs.css";
 import { Carousel } from "react-bootstrap";
+import "../styles/AboutUs.css";
 
 import aboutUsData from "../data/aboutUs.json";
 import historyData from "../data/history.json";
 import Preloader from "../components/preloader";
 
 export default function AboutUs() {
-	const [activeIndex, setActiveIndex] = useState(0);
+	const [activeIndex1, setActiveIndex1] = useState(0);
+	const handleSelect1 = (selectedIndex, e) => {
+		setActiveIndex1(selectedIndex);
+	};
 
-	const handleSelect = (selectedIndex, e) => {
-		setActiveIndex(selectedIndex);
+	const [activeIndex2, setActiveIndex2] = useState(0);
+	const handleSelect2 = (selectedIndex, e) => {
+		setActiveIndex2(selectedIndex);
 	};
 
 	const [loading, setLoading] = useState(true);
@@ -31,39 +34,80 @@ export default function AboutUs() {
 					<div className="Ellipse1" />
 					<div className="Ellipse2" />
 
-					<section className="container text-center ">
+					<section className="container text-center">
 						<div className="second-section">
 							<h1 className="heading2">
 								MISSION, VISION & CORE VALUES
 							</h1>
-							{/* 
-						
-						NOT SECOND CAROUSEL MISSION VISSION CORE 
-						
-						*/}
-							<div className="row services-list justify-content-center">
-								{aboutUsData.map((item, index) => (
-									<div
-										key={index}
-										className={`item-card ${index < 2 ? "col-4" : "col-8"}`}
-									>
-										<div className="service-box">
-											<img
-												className="service-icon"
-												src={item.icon}
-												alt={`${item.title} Icon`}
-											/>
-											<h3 className="text-center">
-												{item.title}
-											</h3>
-											<p
-												dangerouslySetInnerHTML={{
-													__html: item.description,
-												}}
-											/>
+							<div className="d-none d-md-block">
+								<div className="row services-list justify-content-center">
+									{aboutUsData.map((item, index) => (
+										<div
+											key={index}
+											className={`item-card ${index < 2 ? "col-4" : "col-8"}`}
+										>
+											<div className="service-box">
+												<img
+													className="service-icon"
+													src={item.icon}
+													alt={`${item.title} Icon`}
+												/>
+												<h3 className="aboutus-title">
+													{item.title}
+												</h3>
+												<p
+													className="aboutus-desciption"
+													dangerouslySetInnerHTML={{
+														__html: item.description,
+													}}
+												/>
+											</div>
 										</div>
-									</div>
-								))}
+									))}
+								</div>
+							</div>
+							<div className="d-block d-md-none">
+								<Carousel
+									activeIndex={activeIndex1}
+									onSelect={handleSelect1}
+									indicators={false}
+								>
+									{aboutUsData.map((item, index) => (
+										<Carousel.Item key={index}>
+											<div className="item-card">
+												<div className="service-box">
+													<img
+														className="service-icon"
+														src={item.icon}
+														alt={`${item.title} Icon`}
+													/>
+													<h3 className="text-center aboutus-title">
+														{item.title}
+													</h3>
+													<p
+														className="aboutus-description"
+														dangerouslySetInnerHTML={{
+															__html: item.description,
+														}}
+													/>
+												</div>
+											</div>
+										</Carousel.Item>
+									))}
+								</Carousel>
+								<div className="custom-carousel-indicators-1">
+									{aboutUsData.map((_, index) => (
+										<span
+											key={index}
+											className={
+												index === activeIndex1
+													? "carousel-indicator active"
+													: "carousel-indicator"
+											}
+											onClick={() => handleSelect1(index)}
+										></span>
+									))}
+								</div>
 							</div>
 						</div>
 					</section>
@@ -74,14 +118,9 @@ export default function AboutUs() {
 								BEHIND THE PRODUCT
 							</h1>
 							<div className="d-md-none">
-								{/* 
-						
-						CAROUSEL HISTORY 
-						
-						*/}
 								<Carousel
-									activeIndex={activeIndex}
-									onSelect={handleSelect}
+									activeIndex={activeIndex2}
+									onSelect={handleSelect2}
 									className="custom-carousel-2"
 									indicators={false}
 									interval={9000}
@@ -89,10 +128,10 @@ export default function AboutUs() {
 									{historyData.map((item, index) => (
 										<Carousel.Item
 											key={index}
-											className="carousel-item text-center"
+											className="carousel-item aboutus-description"
 										>
 											<p
-												className="description mx"
+												className=" mx"
 												dangerouslySetInnerHTML={{
 													__html: item.content,
 												}}
@@ -105,20 +144,15 @@ export default function AboutUs() {
 										<span
 											key={index}
 											className={
-												index === activeIndex
+												index === activeIndex2
 													? "carousel-indicator active"
 													: "carousel-indicator"
 											}
-											onClick={() => handleSelect(index)}
+											onClick={() => handleSelect2(index)}
 										></span>
 									))}
 								</div>
 							</div>
-							{/* 
-						
-						NOT CAROUSEL HISTORY 
-						
-						*/}
 							<div className="row d-none d-md-flex">
 								<div className="col-12 text-center history-content">
 									{historyData.map((item, index) => (
